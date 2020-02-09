e8ad6f52fe1dcf383b376fb4682366f8bee55faf
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2020-02-08 22:40:53 -0500

    Update extend.php
    Now only targets external links plus compatibility issues with my Pop BBCode and SoundManager2 extension are fixed.

--- extend.php
+++ extend.php
@@ -18,33 +18,43 @@ return [
 <script>
   flarum.core.compat.extend.extend(flarum.core.compat['components/CommentPost'].prototype, 'config', function(output, isInitialized, context) {
     if (context.custommExtLastContentHtml !== context.contentHtml) {
-$('.Post-body a').on('click',function(){
 
-var getLink = $(this).attr('href');
+      $('a').filter(function() {
+        return this.hostname && this.hostname !== location.hostname; 
+        }).addClass('External-link');
 
-swal({
-  title: 'This link will open in a new tab/window.',
-  text: 'No thanks, <a href="' + getLink + '" target="_self">open in current tab</a>.',
-  html: true,
-  type: "warning",
-  confirmButtonColor: '#2ecc71',
-  confirmButtonText: "OK",
-  cancelButtonText: "Cancel",
-  showCancelButton: true,
-  showConfirmButton: true,
-},function(){
+      $('a[href$=".mp3"], a[href$=".ogg"], a[href$=".wav"], a[href$=".mp4"], a[href$=".m4a"], a[href$=".acc"], a[href$=".opus"], a[href$=".flac"]').filter(function() {
+        return this.hostname && this.hostname !== location.hostname; 
+        }).removeClass('External-link');
 
-window.open(getLink,'_blank')
+      $('.External-link').on('click',function(){
 
-});
+        var getLink = $(this).attr('href');
 
-return false;
+        swal({
+                title: 'This link will open in a new tab.',
+                text: 'No thanks, <a href="' + getLink + '" target="_self">open in current tab</a>.',
+                html: true,
+                type: "warning",
+                confirmButtonColor: '#2ecc71',
+                confirmButtonText: "OK, Go!",
+                cancelButtonText: "Cancel",
+                showCancelButton: true,
+                showConfirmButton: true,
+        },function(){
 
-});
+                window.open(getLink,'_blank')
+
+        });
+
+                return false;
+
+        });
     }
     context.custommExtLastContentHtml = context.contentHtml;
   });
 </script>
+
 <script src='/assets/extensions/zerosonesfun-link-decisions/sweetalert.js'></script>
 HTML;
         })
